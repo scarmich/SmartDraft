59aa53725e604a94991891a316cd0e97b2d3a9c0
Tom Powers <powerst@cs.purdue.edu>
2014-02-07 23:47:00 -0500

    frame work update #1
    qweerhyjtrda

--- Src/SmartDraft/Form1.cs
+++ Src/SmartDraft/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace SmartDraft
 {
@@ -21,9 +23,46 @@ namespace SmartDraft
         private void Form1_Load(object sender, EventArgs e)
         {
             
+            ToolTip tip = new ToolTip();
+            this.button2.BackgroundImage = Image.FromFile(@"C:\Users\Tom\Documents\GitHub\SmartDraft\Src\SmartDraft\img\Aatrox.png");
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (StreamReader sr = new StreamReader("champdata.txt"))
+            {
+                String line;
+                // Read and display lines from the file until the end of 
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    sb.AppendLine(line);
+                }
+            }
+            string champdata = sb.ToString();
+            
+            if(radioButton4.Checked){
+                //Solo Lane
+                //run(solo,champdata)
+            }
+            else if(radioButton3.Checked){
+                //ADC
+                //run(adc,champdata)
+            }
+            else if (radioButton2.Checked)
+            {
+                //Jungle
+                //run(jungle,champdata)
+            }
+            else
+            {
+                //Support
+                //run(sup,champdata)
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
