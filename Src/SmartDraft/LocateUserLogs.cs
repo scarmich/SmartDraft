c882729a6c974fb3786830941d386796209c8381
Shawn Carmichael <scarmich@purdue.edu>
2014-02-08 00:37:43 -0500

    Made LocateUserLogs look for logs
    Added message boxes for debugging and directory searching

--- Src/SmartDraft/LocateUserLogs.cs
+++ Src/SmartDraft/LocateUserLogs.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.IO;
+using System.Windows.Forms;
 
 public class LocateUserLogs
 {
-	public LocateUserLogs()
+	public String LocateUserLogs()
 	{
+        if (Directory.Exists("C:\\Riot Games\\League of Legends\\Logs"))
+        {
+            MessageBox.Show("The log files were not detected in their default install location. Please specify the 'Riot Games\\League of Legends\\Logs' directory location.");
+            return "Somewhere else";
+        }
+        else
+        {
+            return "C:\\Riot Games\\League of Legends\\Logs";
+        }
 	}
 }
